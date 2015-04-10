64e124da9dbdee5e571172fa762266591a086285
TMHolen <TorMx99@gmail.com>
2015-04-10 09:31:03 +0200

    Added getters and setters to
    Added the getters and setters we need

--- trackorganizer/backend/HD.java
+++ trackorganizer/backend/HD.java
@@ -4,24 +4,39 @@ import java.util.ArrayList;
 
 /**
  * Represents a hard drive used for storing audio files.
- * 
+ *
  * @author Kristian Honningsvag
  * @version 0.1
  */
+public class HD extends Media {
 
-public class HD extends Media
-{
     // Instance variables
+
     private ArrayList<File> files;
 
     /**
      * Constructor.
-     * 
+     *
      * @param name Name of the hard drive.
      */
-    public HD(String name)
-    {
+    public HD(String name) {
         super(name);
         files = new ArrayList<File>();
+    }
+
+    /**
+     * Returns the files contained on the hard drive.
+     * @return 
+     */
+    public ArrayList<File> getFiles() {
+        return files;
+    }
+
+    /**
+     * Sets the files on the hard drive.
+     * @param files 
+     */
+    public void setFiles(ArrayList<File> files) {
+        this.files = files;
     }
 }
--- trackorganizer/backend/PhysicalRelease.java
+++ trackorganizer/backend/PhysicalRelease.java
@@ -4,13 +4,12 @@ import java.util.ArrayList;
 
 /**
  * Represents physical releases like CD's and vinyl LP's.
- * 
+ *
  * @author Kristian Honningsvag
  * @version 0.1
  */
+public class PhysicalRelease extends Media {
 
-public class PhysicalRelease extends Media
-{
     // Instance variables
     private String artistName;
     private int releaseYear;
@@ -21,12 +20,122 @@ public class PhysicalRelease extends Media
 
     /**
      * Constructor.
-     * 
+     *
      * @param name Name of the physical release.
      */
-    public PhysicalRelease(String name)
-    {
+    public PhysicalRelease(String name) {
         super(name);
         tracks = new ArrayList<Track>();
     }
+
+    //Getters
+    /**
+     * Returns the artist name of the release.
+     *
+     * @return artistName
+     */
+    public String getArtistName() {
+        return artistName;
+    }
+
+    /**
+     * Returns the release year of the release.
+     *
+     * @return releaseYear
+     */
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    /**
+     * Returns the record label of the release.
+     *
+     * @return recordLabel
+     */
+    public String getRecordLabel() {
+        return recordLabel;
+    }
+
+    /**
+     * Returns the total duration of the release.
+     *
+     * @return totalDuration
+     */
+    public int getTotalDuration() {
+        return totalDuration;
+    }
+
+    /**
+     * Returns the tracks contained in the release.
+     *
+     * @return tracks
+     */
+    public ArrayList<Track> getTracks() {
+        return tracks;
+    }
+
+    /**
+     * Returns the archive number the release is stored under.
+     *
+     * @return archiveNR
+     */
+    public int getArchiveNR() {
+        return archiveNR;
+    }
+
+    //Setters
+    /**
+     * Sets the name of the artist who made the release.
+     *
+     * @param artistName
+     */
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+
+    /**
+     * Sets the release year of the release.
+     *
+     * @param releaseYear
+     */
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    /**
+     * Sets the record label of the release.
+     *
+     * @param recordLabel
+     */
+    public void setRecordLabel(String recordLabel) {
+        this.recordLabel = recordLabel;
+    }
+
+    /**
+     * Sets the total duration of the release.
+     *
+     * @param totalDuration
+     */
+    public void setTotalDuration(int totalDuration) {
+        this.totalDuration = totalDuration;
+    }
+
+    /**
+     * Sets the tracks of the release.
+     *
+     * @param tracks
+     */
+    public void setTracks(ArrayList<Track> tracks) {
+        this.tracks = tracks;
+    }
+
+    /**
+     * Sets the archive number the release is stored under.
+     *
+     * @param archiveNR
+     */
+    public void setArchiveNR(int archiveNR) {
+        this.archiveNR = archiveNR;
+    }
+
 }
