ab412e1505718be46bbec326d0b5ac4ac58a063b
alegro91 <alejandro_mtg@hotmail.com>
2015-04-17 09:55:29 +0200

    pruning

--- tests/backend/DurationHandlerTest.java
+++ tests/backend/DurationHandlerTest.java
@@ -5,38 +5,15 @@
  */
 package backend;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- * DESCRIPTION NEEDED
- */
+    /**
+     * DESCRIPTION NEEDED
+     */
 public class DurationHandlerTest {
 
-    public DurationHandlerTest() {
-    }
-
-    @BeforeClass
-    public static void setUpClass() {
-    }
-
-    @AfterClass
-    public static void tearDownClass() {
-    }
-
-    @Before
-    public void setUp() {
-    }
-
-    @After
-    public void tearDown() {
-    }
-
-    /**
+     /**
      * Test of formatDuration method, of class DurationHandler.
      */
     @Test
