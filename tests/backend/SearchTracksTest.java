f30463ad30cdeedbf4f38c45353d04f4d6dcf6cc
Pitmairen <progrper@gmail.com>
2015-04-18 13:26:45 +0200

    Add search tracks by artists

--- tests/backend/SearchTracksTest.java
+++ tests/backend/SearchTracksTest.java
@@ -61,13 +61,55 @@ public class SearchTracksTest {
         
     }
     
+    
+     /**
+     * Test search by artist exact
+     */
+    @Test
+    public void testSearchByArtistExact() {
+        
+        createTracksForSearch();
+        
+        ArrayList<Track> res = to.findTracks(new SearchTracks.ByArtist("artist2").exact());
+
+        assertEquals(1, res.size());
+        assertEquals("ArtisT2", ((MusicTrack)res.get(0)).getArtist());
+        
+        res = to.findTracks(new SearchTracks.ByArtist("non existing").exact());
+
+        assertEquals(0, res.size());
+        
+    }
+    
+    /**
+     * Test search by artist contains
+     */
+    @Test
+    public void testSearchByArtistContains() {
+        
+        createTracksForSearch();
+        
+        ArrayList<Track> res = to.findTracks(new SearchTracks.ByArtist("st1").contains());
+
+        assertEquals(1, res.size());
+ 
+        assertEquals("ArtisT1", ((MusicTrack)res.get(0)).getArtist());
+        
+        res = to.findTracks(new SearchTracks.ByArtist("nonexisting").contains());
+
+        assertEquals(0, res.size());
+        
+    }
+    
+    
+    
     private void createTracksForSearch(){
         
         CD cd = new CD("Album1", "Artist1", Year.of(2014), "Sony", 10001);
         
 
-        cd.addTrack(new MusicTrack("SoNg1", "Artist1", Duration.ofSeconds(100)));
-        cd.addTrack(new MusicTrack("Song NuMber 2", "Artist2", Duration.ofSeconds(100)));
+        cd.addTrack(new MusicTrack("SoNg1", "ArtisT1", Duration.ofSeconds(100)));
+        cd.addTrack(new MusicTrack("Song NuMber 2", "ArtisT2", Duration.ofSeconds(100)));
 
         to.addMedia(cd);
         
--- trackorganizer/backend/SearchTracks.java
+++ trackorganizer/backend/SearchTracks.java
@@ -50,4 +50,57 @@ public class SearchTracks {
             
         }   
     }
+    
+    
+    
+    
+    
+    /**
+     * Container for artist search
+     */
+    public static class ByArtist {
+       
+        private String word;
+        
+        public ByArtist(String word){
+            this.word = word.toLowerCase();
+        }
+    
+        
+        /**
+         * Creates a predicate that check for word contained in artist name.
+         * @return The predicate
+         */
+        public Predicate contains(){
+            
+            return (Object obj) -> {
+                boolean res = false;
+                if (obj instanceof MusicTrack) {
+                    res = ((MusicTrack) obj).getArtist().toLowerCase().contains(word);
+                }
+                return res;
+            };
+            
+        }
+        
+        /**
+         * Creates a predicate that check for exact match with word. (Ignoring case)
+         * @return The predicate
+         */
+        public Predicate exact() {
+            
+            return (Object obj) -> {
+                boolean res = false;
+                if (obj instanceof MusicTrack) {
+                    res = ((MusicTrack) obj).getArtist().toLowerCase().equals(word);
+                }
+                return res;
+            };
+            
+        }   
+    } 
+    
+    
+    
+    
 }
